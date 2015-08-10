6573594885aaee706c1e520d16669e6fdff450f8
konshow <chunpong.p@gmail.com>
2015-08-10 11:14:00 +0700

    refactor

--- src/th/co/gosoft/tdd/Books.java
+++ src/th/co/gosoft/tdd/Books.java
@@ -3,5 +3,6 @@ package th.co.gosoft.tdd;
 public class Books {
 
     public static final int HARRY_POTTER_1 = 1;
+    public static final int HARRY_POTTER_2 = 2;
 
 }
--- src/th/co/gosoft/tdd/TestSalesBooks.java
+++ src/th/co/gosoft/tdd/TestSalesBooks.java
@@ -2,26 +2,26 @@ package th.co.gosoft.tdd;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class TestSalesBooks {
 
+    Baskets baskets = new Baskets();
+    
     @Test
     public void calculateEmptyBaskets(){
-        Baskets baskets = new Baskets();
         assertEquals(0.00D, baskets.calculate(),0);
     }
     
     @Test
     public void calculateOneBookInBaskets(){
-        Baskets baskets = new Baskets();
         baskets.add(Books.HARRY_POTTER_1);
         assertEquals(8.00D, baskets.calculate(),0);
     }
     
     @Test
     public void calculateTwoBookInBaskets(){
-        Baskets baskets = new Baskets();
         baskets.add(Books.HARRY_POTTER_1);
         baskets.add(Books.HARRY_POTTER_1);
         assertEquals(16.00D, baskets.calculate(),0);
@@ -29,10 +29,16 @@ public class TestSalesBooks {
     
     @Test
     public void calculateThreeBookInBaskets(){
-        Baskets baskets = new Baskets();
         baskets.add(Books.HARRY_POTTER_1);
         baskets.add(Books.HARRY_POTTER_1);
         baskets.add(Books.HARRY_POTTER_1);
         assertEquals(24.00D, baskets.calculate(),0);
+    }
+    
+    @Test
+    public void calculateTwoDifferentBookInBaskets(){
+        baskets.add(Books.HARRY_POTTER_1);
+        baskets.add(Books.HARRY_POTTER_2);
+        assertEquals(15.20D, baskets.calculate(),0);
     }
 }
