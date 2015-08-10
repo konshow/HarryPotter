1c6ea3af7cde935da767fce640ff5f99b4d182a7
konshow <chunpong.p@gmail.com>
2015-08-10 11:46:20 +0700

    refactor

--- src/th/co/gosoft/tdd/Baskets.java
+++ src/th/co/gosoft/tdd/Baskets.java
@@ -4,20 +4,14 @@ public class Baskets {
 
     private double pricePerBook = 8.00D;
     private int books[] = new int[6];
-    double[] discount = new double[]{0.00D, 1.00D, 0.95D, 0.90D, 0.80D, 0.75D};
+    private double[] discount = new double[]{0.00D, 1.00D, 0.95D, 0.90D, 0.80D, 0.75D};
+    private double totalPrice = 0.00D;
+    private int bookDifferent = 0;
 
     public double calculate() {
-        double totalPrice = 0.00D;
-        int bookDifferent = 0;
         for (int indexSeries = 0; indexSeries < books.length; indexSeries++) {
             if(books[indexSeries] != 0){
-                bookDifferent = 0;
-                for (int indexBooksEdition = 0; indexBooksEdition < books.length; indexBooksEdition++) {
-                    if (books[indexBooksEdition] != 0) {
-                        bookDifferent++;
-                        books[indexBooksEdition]--;
-                    }
-                }
+                bookDifferent = getDifferentBooks();
                 totalPrice += pricePerBook * bookDifferent * discount[bookDifferent];
                 if(books[indexSeries] != 0){
                     indexSeries--;
@@ -25,6 +19,17 @@ public class Baskets {
             }
         }
         return totalPrice;
+    }
+
+    private int getDifferentBooks() {
+        int bookDifferent = 0;
+        for (int indexBooksEdition = 0; indexBooksEdition < books.length; indexBooksEdition++) {
+            if (books[indexBooksEdition] != 0) {
+                bookDifferent++;
+                books[indexBooksEdition]--;
+            }
+        }
+        return bookDifferent;
     }
 
     public void add(int book) {
